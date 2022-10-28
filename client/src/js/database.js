107ec1bd13442676e380400f48ec036d4411172c
Jared <jjohns3@tulane.edu>
2022-10-28 18:50:58 -0500

    work on database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: id, text: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
