5257cbdac5c113969103939c78a16a2c0089655a
Jared <jjohns3@tulane.edu>
2022-10-28 20:35:28 -0500

    fixed get and put db functions and calls

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,9 @@
 import { openDB } from 'idb';
 
+const version = 3;
+
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate', version, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -13,18 +15,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
-  const jateDb = await openDB('jate', 1);
+export const putDb = async (content) => {
+  console.log(content);
+  const jateDb = await openDB('jate', version);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, text: content });
+  const request = store.put({ text: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', version);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
